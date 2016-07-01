4d90aca82da7b8b632557b9ad79c0951ff5f9157
Jason Crawford <daxapps777@gmail.com>
2016-07-01 04:50:29 -0500

    working through iOS developer course on Udemy

--- My Noughts and Crosses/ViewController.swift
+++ My Noughts and Crosses/ViewController.swift
@@ -9,7 +9,56 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var activePlayer = 1  // 1 = noughts, 2 = crosses
+    
+    var gameState = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    
+    let winningCombinations = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
 
+    @IBOutlet var button: UIButton!
+    
+    
+    @IBAction func buttonPressed(sender: AnyObject) {
+        
+        if (gameState[sender.tag] == 0) {
+            
+            gameState[sender.tag] = activePlayer
+        
+            if activePlayer == 1 {
+                
+                sender.setImage(UIImage(named: "nought.png"), forState: .Normal)
+                
+                activePlayer = 2
+                
+            } else {
+            
+                sender.setImage(UIImage(named: "cross.png"), forState: .Normal)
+                
+                activePlayer = 1
+            }
+            
+            for combination in winningCombinations {
+                
+                if (gameState[combination[0]] != 0 && gameState[combination[0]] == gameState[combination[1]] && gameState[combination[1]] == gameState[combination[2]]) {
+                    
+                    if gameState[combination[0]] == 1 {
+                    
+                        print("Noughts has won!")
+                    
+                    } else {
+                    
+                        print("Crosses has won!")
+                    
+                    }
+                
+                }
+            }
+        }
+        
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
